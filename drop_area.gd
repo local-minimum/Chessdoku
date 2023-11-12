ec103dabd2b9c22b037a08bee0d1c68dc6a63b2d
Martin Zackrisson <martin.zackrisson@fripost.org>
2023-11-12 11:08:35 +0100

    Add hint text fields to drop area

--- drop_area.gd
+++ drop_area.gd
@@ -7,6 +7,9 @@ var occupant: ChessPiece
 var hover_color = Color(Color.MEDIUM_ORCHID, 0.7)
 var box_position: Vector2i
 var box: CDBox
+var _hint_1: RichTextLabel
+var _hint_2: RichTextLabel
+var _sprite: Sprite2D
 	
 func can_occupy(piece: ChessPiece):
 	if occupant != null && occupant == piece:
@@ -25,18 +28,27 @@ func end_occupation(piece: ChessPiece):
 		box.validate(self)
 
 func begin_hover():
-	modulate = hover_color
+	_sprite.modulate = hover_color
 	
 func end_hover():
-	modulate = (
+	_sprite.modulate = (
 		global.tile_tint_black_color 
 		if isBlackTile else global.tile_tint_white_color
 	)
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():	
+	_sprite = find_child("Sprite2D")
+	_hint_1 = find_child("TextLabel_Lower_Left")
+	_hint_1["theme_override_colors/default_color"] = global.black_tile_hint_color  if isBlackTile else global.white_tile_hint_color
+	_hint_2 = find_child("TextLabel_Lower_Right")
+	_hint_2["theme_override_colors/default_color"] = global.black_tile_hint_color  if isBlackTile else global.white_tile_hint_color		
 	end_hover()
+	hint("", "")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func hint_number(left: int, right: int):
+	hint(str(left), str(right))
+	
+func hint(left: String, right: String):
+	_hint_1.text = left
+	_hint_2.text = "[right]%s[/right]" % right
--- global.gd
+++ global.gd
@@ -60,6 +60,8 @@ var white_color = Color.ANTIQUE_WHITE
 var black_color = Color.DARK_SLATE_GRAY
 var tile_tint_white_color = Color(Color.BEIGE, 0.6)
 var tile_tint_black_color = Color(Color.DARK_VIOLET, 0.6)
+var white_tile_hint_color = Color.DARK_VIOLET
+var black_tile_hint_color = Color.BEIGE
 
 # Assist mode
 var show_box_rule_status = true
